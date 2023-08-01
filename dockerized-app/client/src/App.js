91d2daa89595ec0d4fb28dff08ce95635abbaecb
Byron Murillo <67471798+vairome@users.noreply.github.com>
2023-08-01 14:39:24 -0600

    Update App.js

--- dockerized-app/client/src/App.js
+++ dockerized-app/client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Fragment>
         <header className="header">
-          <div className="title">TEST APP 28-7-2023</div>
+          <div className="title">TEST APP 31-8-2023</div>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page</Link>
         </header>
